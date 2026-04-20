9ffab1273350bee85ec288e8ea170f864f42761f
Ryan Theodore The <ryan.the.2006@gmail.com>
2026-04-20 13:42:17 +0000

    fix: adjust logging level for httpx to reduce spamming logs

--- autoso/bot/main.py
+++ autoso/bot/main.py
@@ -8,6 +8,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
 def main():
